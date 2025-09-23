77b5d86bbdd5d3f163873f4fb5155c481f4c5231
ganesh more <ganeshmore775593@gmail.com>
2025-09-23 13:57:41 +0530

    DSA (Practice on opeartors)

---
+++ day08.cpp
@@ -0,0 +1,59 @@
+// // Eligible 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   int a = 18;
+//   if (a >= 18 ){
+//     cout<<"You are eligible";
+//   }
+//   return 0;
+// }
+// Roll No
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   int rollNo;
+//   cout<<"Enter Your Roll No: "<<endl;
+//   cin>>rollNo;
+//   if (rollNo>30)
+//   {
+//     cout<<"your batch is A";
+//   }
+//   return 0;
+// }
+// Check If a number is positive------------------
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   int n;
+//   cout<<"Enter Number :"<<endl;
+//   cin>>n;
+
+//   if (n>0)
+//   {
+//   cout<<"positive";
+//   }
+//   return 0;
+// }
+
+// Check if a number is negative
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cout<<"Enter user number :";
+  cin>>n;
+
+  if (n<0)
+  {
+    cout<<"Number is Negative";
+  }
+  
+}
